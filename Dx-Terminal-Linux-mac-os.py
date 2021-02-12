400d26559859d9e6e876108677d8759f049572d1
Dx$per <66487530+DxSper@users.noreply.github.com>
2021-02-12 22:38:38 +0100

    Create Dx-Terminal-Linux-mac-os.py

---
+++ Dx-Terminal-Linux-mac-os.py
@@ -0,0 +1,11 @@
+import os
+from simple_term_menu import TerminalMenu
+
+def main():
+    terminal_menu = TerminalMenu(["obfuscate code", "compile python to exe", "run Dx-RAT"])
+    terminal_menu = TerminalMenu(fruits, title="Dx-RAT Terminal")
+    menu_entry_index = terminal_menu.show()
+
+
+if __name__ == "__main__":
+    main()
